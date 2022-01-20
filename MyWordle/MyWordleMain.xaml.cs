583083c5641d3bca097de345b5db59b86671bcf3
Calvin Hsia <calvinh@microsoft.com>
2022-01-19 18:08:14 -0800

    lib

--- MyWordle/MyWordleMain.xaml.cs
+++ MyWordle/MyWordleMain.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using MyWordleLib;
 
 namespace MyWordle
 {
@@ -22,41 +23,11 @@ namespace MyWordle
         public MyWordleMain()
         {
             InitializeComponent();
-            var dicSmall = new DictionaryLib.DictionaryLib(DictionaryLib.DictionaryType.Small); // 5 LTR WORDS = 3425, 2329 uniq
-            var hashSmall = GetWords(dicSmall, DesiredWordLen: 5);
+            var hashSmall = MyWordleLib.MyWordleLib.GetWords(DictionaryLib.DictionaryType.Small, DesiredWordLen: 5);// 5 LTR WORDS = 3425, 2329 uniq
 
-            var dicLarge = new DictionaryLib.DictionaryLib(DictionaryLib.DictionaryType.Large); // 9908, 6406 uniq
-            var hashLarge = GetWords(dicLarge, DesiredWordLen: 5);
+            var hashLarge = MyWordleLib.MyWordleLib.GetWords(DictionaryLib.DictionaryType.Large, DesiredWordLen: 5);// 9908, 6406 uniq
             var x = hashLarge.ElementAt(2);
-
         }
 
-        private HashSet<string> GetWords(DictionaryLib.DictionaryLib dic, int DesiredWordLen, bool UniqueLtrs = true)
-        {
-            var hashResult = new HashSet<string>();
-            var wrd = dic.SeekWord("a");
-            while (!string.IsNullOrEmpty(wrd))
-            {
-                if (wrd.Length == DesiredWordLen)
-                {
-
-                    var uniqueCnt = 0;
-                    if (UniqueLtrs)
-                    {
-                        uniqueCnt = wrd.GroupBy(l => l).Count();
-                    }
-                    else
-                    {
-                        uniqueCnt = DesiredWordLen;
-                    }
-                    if (uniqueCnt == DesiredWordLen)
-                    {
-                        hashResult.Add(wrd);
-                    }
-                }
-                wrd = dic.GetNextWord();
-            }
-            return hashResult;
-        }
     }
 }
---
+++ MyWordleLib/MyWordleLib.cs
@@ -0,0 +1,33 @@
+﻿namespace MyWordleLib
+{
+    public class MyWordleLib
+    {
+        public static HashSet<string> GetWords(DictionaryLib.DictionaryType dictType, int DesiredWordLen, bool UniqueLtrs = true, Random random = null)
+        {
+            var dic = new DictionaryLib.DictionaryLib(dictType, random);
+            var hashResult = new HashSet<string>();
+            var wrd = dic.SeekWord("a");
+            while (!string.IsNullOrEmpty(wrd))
+            {
+                if (wrd.Length == DesiredWordLen)
+                {
+                    var uniqueCnt = 0;
+                    if (UniqueLtrs)
+                    {
+                        uniqueCnt = wrd.GroupBy(l => l).Count();
+                    }
+                    else
+                    {
+                        uniqueCnt = DesiredWordLen;
+                    }
+                    if (uniqueCnt == DesiredWordLen)
+                    {
+                        hashResult.Add(wrd);
+                    }
+                }
+                wrd = dic.GetNextWord();
+            }
+            return hashResult;
+        }
+    }
+}
